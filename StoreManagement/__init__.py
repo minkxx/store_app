90ba81fa585aa4c2c00f463967a49eab9c43a634
minkxx <aryuokk@gmail.com>
2023-10-31 12:09:26 +0530

    minor changes

--- StoreManagement/__init__.py
+++ StoreManagement/__init__.py
@@ -7,82 +7,6 @@ from PIL import Image, ImageTk
 from StoreManagement.logger import log
 from StoreManagement.database import Database
 
-class ctkGUI:
-    def __init__(self):
-        self.db = Database()
-        log.info("starting app")
-
-        # theme settings ---------------------------------------------------------------------------------------------
-        ctk.set_appearance_mode("dark")
-        ctk.set_default_color_theme("dark-blue")
-
-        # root frame settings ----------------------------------------------------------------------------------------
-        self.root = ctk.CTk()
-        self.root.geometry("1200x675")
-        self.root.title("Inventory App")
-        self.root.wm_iconbitmap("StoreManagement//resources//management.ico")
-
-        # data variables ---------------------------------------------------------------------------------------------
-        self.output_var = ctk.StringVar()
-
-        # login frame ------------------------------------------------------------------------------------------------
-        self.login_frame = ctk.CTkFrame(master=self.root)
-        self.login_frame.propagate(False)
-        self.login_frame.pack(padx=300, pady=68, fill="both", expand=True)
-
-        self.title_text = ctk.CTkLabel(master=self.login_frame, text="Login", font=("Microsoft YaHei UI Bold", 20))
-        self.title_text.pack(pady=10)
-
-        self.username_entry = ctk.CTkEntry(master=self.login_frame, placeholder_text="username", font=("Microsoft YaHei UI", 12))
-        self.username_entry.pack(pady=10)
-
-        self.password_entry = ctk.CTkEntry(master=self.login_frame, placeholder_text="password", font=("Microsoft YaHei UI", 12), show="*")
-        self.password_entry.pack(pady=10)
-
-        self.login_button =ctk.CTkButton(master=self.login_frame, text="Login", font=("Microsoft YaHei UI", 12), command=self.loginCmd)
-        self.login_button.pack(pady=10)
-
-        self.output_text = ctk.CTkLabel(master=self.login_frame, text="output", textvariable=self.output_var,font=("Microsoft YaHei UI", 15))
-        # self.output_text.place(x=270, y=200)
-        self.output_text.pack()
-
-        # home frame --------------------------------------------------------------------------------------------------
-        # self.home_frame = ctk.CTkFrame(master=self.root)
-        # self.home_frame.propagate(False)
-        # self.tabs= ctk.CTkTabview(master=self.home_frame)
-        # self.tabs.add("Tab1")
-        # self.tabs.add("Tab2")
-        # self.tabs.add("Tab3")
-        # self.sample_text = ctk.CTkLabel(master=self.tabs.tab("Tab1"), text="Blah blah.")
-        # self.sample_text.pack()
-        # self.sample_text = ctk.CTkLabel(master=self.tabs.tab("Tab2"), text="Blah blah.")
-        # self.sample_text.pack()
-        # self.sample_text = ctk.CTkLabel(master=self.tabs.tab("Tab3"), text="Blah blah.")
-        # self.sample_text.pack()
-
-        # self.home_frame.pack(fill="both", expand=True)
-
-    def loginCmd(self):
-        entered_password = self.password_entry.get()
-        entered_username = self.username_entry.get()
-        
-        cred = self.db.getData("cred")
-        db_username = cred["cred"]["username"]
-        db_password = cred["cred"]["password"]
-        
-        if entered_username == db_username and entered_password == db_password:
-            self.output_var.set("Combination matched!!")
-        elif entered_username != db_username:
-            self.output_var.set("username incorrect!")
-        elif entered_password != db_password:
-            self.output_var.set("password incorrect!")
-        else:
-            self.output_var.set("idk!!")
-
-    def start(self):
-        log.info("app started")
-        self.root.mainloop()
-
 
 class ttkGUI:
     def __init__(self):
@@ -101,13 +25,16 @@ class ttkGUI:
         self.output_var = tk.StringVar()
         self.username_var = tk.StringVar()
         self.password_var = tk.StringVar()
+        self.add_name_var = tk.StringVar()
+        self.add_item_var = tk.StringVar()
+        self.add_quantity_var = tk.IntVar()
 
         # login frame settings
         self.log_frame = ttk.Frame(master=self.root)
         self.log_frame.pack(expand=True, fill="both")
 
         ## login sub frames settings
-        self.frame1 = tk.Frame(master=self.log_frame, borderwidth=1, relief="solid")
+        self.frame1 = tk.Frame(master=self.log_frame)
         self.frame2 = tk.Frame(master=self.log_frame)
         self.frame1.place(x=0, y=0, relwidth=0.5, relheight=1)
         self.frame2.place(relx=0.5, y=0, relwidth=0.5, relheight=1)
@@ -178,13 +105,13 @@ class ttkGUI:
         self.menu_frame.place(x=0, y=35, relwidth=0.2, relheight=1)
  
         ### menu frame widgets
-        self.displayItems_button = ttk.Button(master=self.menu_frame, text="Display Items", width=24, command=self.displayI_frame)
+        self.displayItems_button = ttk.Button(master=self.menu_frame, text="Display Items", width=22, command=self.displayI_frame)
         self.displayItems_button.place(x=0, y=10)
-        self.addItems_button = ttk.Button(master=self.menu_frame, text="Add Items", width=24, command=self.addI_frame)
+        self.addItems_button = ttk.Button(master=self.menu_frame, text="Add Items", width=22, command=self.addI_frame)
         self.addItems_button.place(x=0, y=50)
-        self.removeItems_button = ttk.Button(master=self.menu_frame, text="Remove Items", width=24, command=self.removeI_frame)
+        self.removeItems_button = ttk.Button(master=self.menu_frame, text="Remove Items", width=22, command=self.removeI_frame)
         self.removeItems_button.place(x=0, y=90)
-        self.xItems_button = ttk.Button(master=self.menu_frame, text="X Items", width=24, command=self.xI_frame)
+        self.xItems_button = ttk.Button(master=self.menu_frame, text="X Items", width=22, command=self.xI_frame)
         self.xItems_button.place(x=0, y=130)
         self.yItems_button = ttk.Button(master=self.menu_frame, text="Log out", command=self.prevPage, width=24)
         self.yItems_button.place(x=0, y=380)
@@ -207,30 +134,46 @@ class ttkGUI:
         self.addItems_frame.place(x=170, y=45, width=620, height=400)
         self.text_label2 = ttk.Label(master=self.addItems_frame, text="Add Items in Inventory", font=("Microsoft HaYei UI Bold", 20))
         self.text_label2.pack()
-        self.name_entry = ttk.Entry(master=self.addItems_frame)
-        self.name_entry.pack()
-        self.item_name_entry = ttk.Entry(master=self.addItems_frame)
-        self.item_name_entry.pack()
-        self.quantity_entry = ttk.Entry(master=self.addItems_frame)
-        self.quantity_entry.pack()
-        self.add_item_button = ttk.Button(master=self.addItems_frame, text="Add Item", command=self.add_data)
-        self.add_item_button.pack()
+        self.add_name_label = ttk.Label(master=self.addItems_frame, text="Name", font=("Microsoft HaYei UI Light", 10))
+        self.add_name_label.place(x=220,y=76)
+        self.add_item_name_label = ttk.Label(master=self.addItems_frame, text="Item name", font=("Microsoft HaYei UI Light", 10))
+        self.add_item_name_label.place(x=200,y=136)
+        self.add_quantity_label = ttk.Label(master=self.addItems_frame, text="Quantity", font=("Microsoft HaYei UI Light", 10))
+        self.add_quantity_label.place(x=210,y=196)
+        self.name_entry = ttk.Entry(master=self.addItems_frame, textvariable=self.add_name_var)
+        self.name_entry.place(x=275, y=75)
+        self.item_name_entry = ttk.Entry(master=self.addItems_frame, textvariable=self.add_item_var)
+        self.item_name_entry.place(x=275, y=135)
+        self.quantity_entry = ttk.Entry(master=self.addItems_frame, textvariable=self.add_quantity_var)
+        self.quantity_entry.place(x=275,y=196)
+        self.add_item_button = ttk.Button(master=self.addItems_frame, text="Add Item", command=self.add_data, width=12)
+        self.add_item_button.place(x=270, y=256)
 
     def add_data(self):
-        self.db.insertData(self.name_entry.get(), self.item_name_entry.get(), int(self.quantity_entry.get()))        
+        self.db.insertData(self.add_name_var.get(), self.add_item_var.get(), self.add_quantity_var.get())      
 
     def removeI_frame(self):
-        self.remove_frame = ttk.Frame(master=self.home_frame)
+        self.remove_frame = ttk.Frame(master=self.home_frame, borderwidth=2, relief="solid")
         self.remove_frame.place(x=170, y=45, width=620, height=400)
-        self.text_label3 = ttk.Label(master=self.addItems_frame, text="Remove Items from Inventory", font=("Microsoft HaYei UI Bold", 20))
+        self.text_label3 = ttk.Label(master=self.remove_frame, text="Remove Items from Inventory", font=("Microsoft HaYei UI Bold", 20))
         self.text_label3.pack()
-        self.name_entry = ttk.Entry(master=self.addItems_frame)
-        self.name_entry.pack()
-        self.item_name_entry = ttk.Entry(master=self.addItems_frame)
-        self.item_name_entry.pack()
+        self.remove_name_label = ttk.Label(master=self.remove_frame, text="Name", font=("Microsoft HaYei UI Light", 10))
+        self.remove_name_label.place(x=220,y=76)
+        self.remove_item_name_label = ttk.Label(master=self.remove_frame, text="Item name", font=("Microsoft HaYei UI Light", 10))
+        self.remove_item_name_label.place(x=200,y=136)
+        self.remove_quantity_label = ttk.Label(master=self.remove_frame, text="Quantity", font=("Microsoft HaYei UI Light", 10))
+        self.remove_quantity_label.place(x=210,y=196)
+        self.remove_name_entry = ttk.Entry(master=self.remove_frame)
+        self.remove_name_entry.place(x=275, y=75)
+        self.remove_item_name_entry = ttk.Entry(master=self.remove_frame)
+        self.remove_item_name_entry.place(x=275, y=135)
+        self.remove_quantity_entry = ttk.Entry(master=self.remove_frame)
+        self.remove_quantity_entry.place(x=275,y=196)
+        self.remove_item_button = ttk.Button(master=self.remove_frame, text="Remove Item", command=self.add_data)
+        self.remove_item_button.place(x=270, y=256)
 
     def xI_frame(self):
-        self.x_frame = ttk.Frame(master=self.home_frame)
+        self.x_frame = ttk.Frame(master=self.home_frame, borderwidth=2, relief="solid")
         self.x_frame.place(x=170, y=45, width=620, height=400)
         self.testlabel4 = ttk.Label(master=self.x_frame, text="Just a text", font=("Microsoft HaYei UI Bold", 20))
         self.testlabel4.pack()
@@ -247,6 +190,9 @@ class ttkGUI:
         self.output_var.set("")
         self.username_var.set("")
         self.password_var.set("")
+        self.add_item_var.set("")
+        self.add_name_var.set("")
+        self.add_quantity_var.set("")
         if not self.count == 0:
             for f in self.frames:
                 f.pack_forget()
--- StoreManagement/__main__.py
+++ StoreManagement/__main__.py
@@ -1,6 +1,4 @@
 from StoreManagement import ttkGUI
 from StoreManagement.database import Database
 
-# db = Database()
-
 ttkGUI().start()
